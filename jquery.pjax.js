dcbe6dc6a00364c274399129aa428229870fd828
Stephen Thompson <scthompson@npr.org>
2015-09-24 16:22:13 -0400

    SFW-12069: Fix bug because container.contents is a jQuery whereas container.data is the property I wanted which is the stringized response.

--- jquery.pjax.js
+++ jquery.pjax.js
@@ -267,7 +267,7 @@ function pjax(options) {
       container.url = url.href
     }
 
-    var $head = $(parseHTML(container.contents.match(/<head[^>]*>([\s\S.]*)<\/head>/i)[0]))
+    var $head = $(parseHTML(container.data.match(/<head[^>]*>([\s\S.]*)<\/head>/i)[0]))
 
     // If there is a layout version mismatch, hard load the new url
     if (currentVersion && latestVersion && currentVersion !== latestVersion) {
